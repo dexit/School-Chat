f32338c6bfbd0cc0df457ca49abffbd151bac0dc
KönigM <KnigM@WWS-E109-004.WVSI.local>
2012-11-21 10:04:36 +0100

    ChatServerGUI entfernt. ChatServer bearbeitet..Anmeldefenster verschwindet nach connect... ChatClientGUI Layoutmanager eingebaut. LOG4J eingebaut... Mario

--- src/school/chat/ChatClientGUI.java
+++ src/school/chat/ChatClientGUI.java
@@ -15,11 +15,12 @@ public class ChatClientGUI extends JFrame {
 	// Anfang Variablen
 
 	// Anfang Attribute
-	private JTextField clientPort = new JTextField();
-	private JLabel clientPortLabel = new JLabel();
-	private JTextField clientIP = new JTextField();
-	private JLabel clientIPLabel = new JLabel();
-	private JButton joinServer = new JButton();
+	private JTextField clientPort = new JTextField("12345");
+	private JLabel clientPortLabel = new JLabel("Port");
+	private JTextField clientIP = new JTextField("schoolchat.is-a-geek.org");
+	private JLabel clientIPLabel = new JLabel("ServerIP:");
+	private JButton joinServer = new JButton("join Server");
+        private Font font=new Font("MS Sans Serif", Font.PLAIN, 13);
 	// Ende Attribute
 
 	// Ende Variablen
@@ -36,27 +37,20 @@ public class ChatClientGUI extends JFrame {
 		int y = (d.height - getSize().height) / 2 ;
 		setLocation(x, y);
 		Container cp = getContentPane();
-		cp.setLayout(null);
+                //Layouts genutzt :)
+		cp.setLayout(new BorderLayout());
+                JPanel center=new JPanel(new GridLayout(4, 1));
 		setIconImage(new ImageIcon(getClass().getResource("/icons/comments.png")).getImage());
 		// Anfang Komponenten
 
-		clientPortLabel.setBounds(0, 0, frameWidth, 16);
-		clientPortLabel.setText("Port:");
-		clientPortLabel.setFont(new Font("MS Sans Serif", Font.PLAIN, 13));
-		cp.add(clientPortLabel);
-		clientPort.setBounds(0, 16, frameWidth, 16);
-		clientPort.setText("10243");//"12345");
-		cp.add(clientPort);
-		clientIPLabel.setBounds(0, 32, frameWidth, 16);
-		clientIPLabel.setText("ServerIP:");
-		clientIPLabel.setFont(new Font("MS Sans Serif", Font.PLAIN, 13));
-		cp.add(clientIPLabel);
-		clientIP.setBounds(0, 48, frameWidth, 16);
-		clientIP.setText("schoolchat.is-a-geek.org");//"localhost");
-		cp.add(clientIP);
-		joinServer.setBounds(0, 64, frameWidth, 25);
-		joinServer.setText("join Server");
-		cp.add(joinServer);
+		clientPortLabel.setFont(font);
+		center.add(clientPortLabel);
+		center.add(clientPort);
+		clientIPLabel.setFont(font);
+		center.add(clientIPLabel);
+		center.add(clientIP);
+                cp.add(center,BorderLayout.CENTER);
+		cp.add(joinServer,BorderLayout.SOUTH);
 		joinServer.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent evt) {
 				joinServer(evt);
@@ -73,14 +67,11 @@ public class ChatClientGUI extends JFrame {
 
 	// Anfang Ereignisprozeduren
 	public void joinServer(ActionEvent evt) {
-		school.chat.ChatClient.impMain(this.clientIP.getText(),Integer.parseInt(this.clientPort.getText()));
+		ChatClient.impMain(this.clientIP.getText(),Integer.parseInt(this.clientPort.getText()));
+                setVisible(false);
 	}
 
 	// Ende Ereignisprozeduren
-
-	public static void main(String[] args) {
-		new ChatClientGUI("ChatClient");
-	}
 	// Ende Methoden
 }
 
